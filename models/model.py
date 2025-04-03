637c0f9357bca842e07850e3ca7c3a2b67e2dc96
Shreyas Raman <shreyas_sundara_raman@brown.edu>
2025-04-03 15:47:24 -0400

    super classing and multiple abstract class setup

--- models/model.py
+++ models/model.py
@@ -8,14 +8,19 @@ if TYPE_CHECKING:
     import numpy as np
     from torch import Tensor
 
-class VisualLanguageModel(ABC):
-
+class Model(ABC):
     def __init__(self):
         pass
 
     @abstractmethod
     def preprocess_data(self, data_stream:"Tensor", text_input:Optional[str]):
         pass
+
+class VisionLanguageModel(Model, ABC):
+    def __init__(self):
+        super().__init__()
+        self.to_pil_image = transforms.ToPILImage()
+        
     
     @abstractmethod
     def run_inference(self, data_stream:"Tensor", **kwargs):
@@ -24,16 +29,24 @@ class VisualLanguageModel(ABC):
     #NOTE: optional to override, not all models have to be finetuned
     def finetune_model(self, dataloader: "DataLoader"):
         pass
+    
+    def convert_to_pil_image(self, torch_tensor:torch.Tensor):
+        # Convert NumPy array to PIL Image
+        return self.to_pil_image(torch_tensor)
 
-class LanguageModel(ABC):
+    # Function to convert a PIL image to Base64
+    def pil_to_base64(self, image:Image):
+        buffered = io.BytesIO()
+        image.save(buffered, format="PNG")  # Ensure a supported format like PNG or JPEG
+        return base64.b64encode(buffered.getvalue()).decode("utf-8")
+    
+
+class LanguageModel(Model, ABC):
 
     def __init__(self):
+        super().__init__()
         pass
 
-    @abstractmethod
-    def preprocess_data(self, query:str):
-        pass
-    
     @abstractmethod
     def run_inference(self, query:str, **kwargs):
         pass
